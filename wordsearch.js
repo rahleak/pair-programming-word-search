8ab32d5ac9d1f3f0281ab6cdb295b26205e41dd2
vagrant <rah.leak@gmail.com>
2021-12-02 01:29:44 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+   
+    const apple = horizontalJoin.join('')
+
+    let newArr = []
+    let currentArr = []
+
+    for (i = 0; i < letters[0].length; i++) {
+        for (j = 0; j < letters.length; j++) {
+            currentArr.push(letters[j][i])
+        }
+        newArr.push(currentArr);
+        currentArr = [];
     }
+    const abc = newArr.map(lv => lv.join(''))
+    const orange = abc.join('')
+    if (orange.includes(word) || apple.includes(word)) {
+        return true
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports =  wordSearch;
